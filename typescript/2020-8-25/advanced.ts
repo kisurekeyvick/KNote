c8dd6d6d48a0ab838d34f666633a0f9f309a18d8
kisurekeyvick <zttaijue1207@163.com>
2020-08-25 18:22:03 +0800

    feat(*)

---
+++ typescript/2020-8-25/advanced.ts
@@ -0,0 +1,23 @@
+/**
+ * TS 中的基础类型
+ * 
+ * boolean
+ * number
+ * string
+ * array
+ * tuple
+ * enum
+ * void
+ * null & undefined
+ * any & unknown
+ * never
+ */
+
+/**
+ * 1.any 和 unknown 的区别
+ * 
+ * any: 任意类型
+ * unknown: 未知的类型
+ */
+
+
